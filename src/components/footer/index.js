8e1deebbbbbc3bffbfe2b6fa1122780a05698a1c
Samuel Pitoňák <sampittko@gmail.com>
2021-01-07 00:12:33 +0100

    Improve visuals

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -27,19 +27,22 @@ const Footer = () => {
   const { thesis } = data.site.siteMetadata
 
   return (
-    <footer className="text-center md:text-left">
-      <a
-        href={thesis.link}
-        rel="noreferrer"
-        className="font-mono font-bold text-amber-800 hover:text-amber-900"
-      >
-        {thesis.text}
-      </a>
-      <Socials
-        {...socials}
-        className="flex justify-center pt-6 space-x-6 md:pt-8 md:order-2"
-      />
-    </footer>
+    <>
+      <footer className="z-10 text-center md:text-left">
+        <a
+          href={thesis.link}
+          rel="noreferrer"
+          className="font-mono font-bold animate-pulse text-amber-800 hover:text-amber-900"
+        >
+          {thesis.text}
+        </a>
+        <Socials
+          {...socials}
+          className="flex justify-center pt-6 space-x-6 md:pt-8 md:order-2"
+        />
+      </footer>
+      <div className="absolute bottom-0 left-0 z-0 w-screen bg-gray-50 h-1/3" />
+    </>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import Footer from "./footer/"
 
 const Layout = ({ children }) => (
-  <div className="flex flex-col items-center justify-between min-h-screen p-10 md:items-start max-w-screen bg-gray-50 md:p-20 lg:p-24">
+  <div className="flex flex-col items-center justify-between min-h-screen p-10 md:items-start max-w-screen bg-gradient-to-br from-gray-100 to-gray-50 md:p-20 lg:p-24">
     <main>{children}</main>
     <Footer />
   </div>
